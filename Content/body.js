aa00bed9acd9f9f5c30d3c7ae25fe028caf73c6d
Nikola <nikolajharris@gmail.com>
2019-01-12 22:06:03 -0500

    HTML outline

---
+++ Content/body.js
@@ -0,0 +1,50 @@
+const bodyHeadline = 'What you get with a subscription to Money Calendar Pro';
+const body = 'Suppose you knew ahead of time that just one of the top 325 stocks or ETFs was expected to jump an average of $1.75 per share on a specific date, and that move was going to take place over a precisely detailed period of time?\n\
+Taking your knowledge base one step further, suppose you knew that this “pattern” of profit happened in nine out of the last 10 years?\n\
+Think about how much this trading advantage could mean to you… just how much it could move the needle on your existing portfolio – indeed, what it could mean to your retirement planning.\n\
+Now you can have access to this exact type of information… every single day of the calendar year.\n\
+This first-mover advantage is worth millions. It’s the kind of head-start the Wall Street fat-cats don’t want you to have, and it’s stuff nobody else has dreamed of, let alone seen.\n\
+It’s available now, and it will both pull back the curtain on how real traders work, and lead you to potential gains of 100%… 200%… and beyond on even the smallest stock market moves.\n\
+AMERICA\'S #1 PATTERN TRADER IS BACK\n\
+It doesn’t seem possible to get the kind of detailed information necessary to “trade ahead” of stock price moves. But when you’ve spent five years crunching over 328 million data points on 3,000 stocks, looking for patterns that happen 90 to 100% of the time for the last 10 years, you’ve earned the right to a moneymaking tool to call your own.\n\
+That’s where Tom Gentile’s Money Calendar Pro comes in.\n\
+Tom Gentile, America’s #1 Pattern Trader, is now a member of the Money Map team. He’s going to show you how he uses his proprietary Money Calendar tool to turn information on the top 325 stocks and ETFs into potential “payout appointments” you can literally schedule on your calendar.\n\
+After a successful and lucrative career in which he taught over 300,000 people his secrets on trading, co-authored four books, and founded – and sold – the world’s most trusted financial education company, Tom’s back to help Money Map subscribers reach their financial goals.\n\
+Here are just a few examples of the kind of gains Tom found with his Money Calendar:\n\
+A 100% gain on GameStop Corp. (GME) in 10 days;\n\
+A 111% gain on Amazon.com, Inc. (AMZN) in just 7 days,\n\
+A 123% gain on Apple Inc. (AAPL) in 11 days,\n\
+And a 229% gain on Priceline (PCLN) in under 12 days.\n\
+These are trades that could easily turn investments as little as $500 into $1,000 within 30 days.\n\
+MONEY CALENDAR PRO\n\
+Each week, Money Calendar Pro reveals a trade right in Tom’s wheelhouse: a minimum 100% return possibility. That’s right: Tom won’t even consider a trade unless it has the chance to pay out with a “double.” It’s part of his “Money Doubler” strategy that’s at the heart of the Money Calendar Pro.\n\
+Tom’s recommendations come with step-by-step video instructions on how to identify and place his trades. In addition, Tom keeps track of every one of his trade recommendations, helping you determine a correct course of action while the trade is active, including “profit-taking” exit strategies.\n\
+$5,085 in 10 Days\n\
+We want to introduce you to our most surefire way to pick up extra cash in 2018…\n\
+And show how it can help you collect $162,000 from a small, one-time stake.\n\
+Now, here’s why it’s so important you don’t wait to take part in this.\n\
+We’ve seen the cash move very quickly with fast-moving profit potential from paydays like…\n\
+$2,000 on IBM in 1 day…\n\
+$2,000 on AAPL in 8 days…\n\
+$2,837 on NAV in 7 days…\n\
+$4,385 in GOOGL in 10 days…\n\
+$2,000 on GILD in 1 day, and…\n\
+$5,085 on JNJ in 10 days.\n\
+You don’t need any experience either.\n\
+It only takes about 10 minutes for you to get set up.'
+
+document.getElementById('bodyHeadline').innerHTML = bodyHeadline;
+document.getElementById('body').innerHTML = body;
+
+const sideBodyHead = 'WHAT YOU GET WITH A SUBSCRIPTION TO MONEY CALENDAR PRO';
+const sideBody = 'All winners so far! — Yadev Pravat\
+You pick so many more winners... it blows me away, and your winners are big winners. — Tuck Fiddler\
+\n\
+"I\'ve made $200,000.00 on Tom Gentile\'s Money Calendar Alert" — Thomas Mackey\
+\n\
+Tom\'s system is very unique and very user friendly. The best I have seen. — Ryder Tillman\
+\n\
+$1,124 in 6 calendar days and $740 in 9 calendar days! — Ryan Stevens';
+
+document.getElementById('sideBodyHead').innerHTML = sideBodyHead;
+document.getElementById('sideBody').innerHTML = sideBody;
