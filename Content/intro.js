ddc5e7ebf5aa995da632e9da0a854ad26d02e32c
Nikola <nikolajharris@gmail.com>
2019-01-13 21:55:15 -0500

    Style Update

--- Content/intro.js
+++ Content/intro.js
@@ -1,12 +1,15 @@
 const headline = 'Money Calendar Pro';
-const intro = 'Tom Gentile didn’t get to be “America’s #1 Pattern Trader” for nothing: he’s earned it.\
-After teaching over 300,000 people his secrets to spotting high-probability, low-risk trades, co-authoring twelve books, and running – then selling for millions – one of the most trusted financial education company’s in the world, Tom’s credentials are unquestioned…\
-And now, after spending the last five years developing the most powerful market analysis and trading tool on the market, Tom’s showing readers how to capture gains of 100%… 200%… and beyond to you today through his remarkable Money Calendar tool.\
+
+const intro = 'Tom Gentile didn’t get to be “America’s #1 Pattern Trader” for nothing: he’s earned it.\n\
+After teaching over 300,000 people his secrets to spotting high-probability, low-risk trades, co-authoring twelve books, and running – then selling for millions – one of the most trusted financial education company’s in the world, Tom’s credentials are unquestioned…'
+
+const introBody = 'And now, after spending the last five years developing the most powerful market analysis and trading tool on the market, Tom’s showing readers how to capture gains of 100%… 200%… and beyond to you today through his remarkable Money Calendar tool.\
 The Money Calendar is Tom’s proprietary system that crunches through 10 years of daily transactions, not only on the best ETFs, but also on more than 300 of the world’s most liquid stocks – like a codebreaker. He sifts through extraordinary patterns nobody else can see, that repeat over and over again with a 90-100% consistency. As if that wasn’t enough, Tom also uses a power meter to pinpoint only the patterns that are getting stronger – give you the best possible trade.\
 It’s that consistency that allows Tom to “schedule” gains on his calendar… like potential “Payout Appointments” that hit within 30 days’ time.';
 
 document.getElementById('headline').innerHTML = headline;
 document.getElementById('intro').innerHTML = intro;
+document.getElementById('introBody').innerHTML = introBody;
 
 const subHeadline = 'GIANT CASH OPPORTUNITIES..';
 const subIntro = 'This simple strategy gives you the opportunity to collect money every 15 days on average… resulting in a yearly income of $162,000. Even better, it only takes about 10 minutes for you to get set up. It’s like having a “money machine” that has cranked out tons of cash opportunities week after week.'
